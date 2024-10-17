8edc96b871f89bcf444efb63dca620922116f9c6
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-17 15:24:32 +0100

    created models

--- fintech_app/admin.py
+++ fintech_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import WalletType, Wallet, Transaction
 
 # Register your models here.
+admin.site.register(WalletType)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
--- fintech_app/models.py
+++ fintech_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class WalletType(models.Model):
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+    	return self.type
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_type = models.ForeignKey(WalletType, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+    def __str__(self):
+    	return f'{self.user.username} wallet'
+
+
+class Transaction(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	description = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.wallet.user.username
