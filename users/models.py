b0a69d44a02a8a69b2cff0eb24b37df6af1ca217
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-18 07:26:30 +0100

    created/auto-create profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from fintech_app.models import Transaction
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_no = models.CharField(max_length=255)
+    date_of_birth=models.DateTimeField(auto_now=True)
+    profile_image = models.ImageField(upload_to='profile_images', null=True, blank=True)
+    profile_bio=models.TextField(null=True, blank=True, max_length=500)
+    transactions = models.ManyToManyField(Transaction, symmetrical=False, blank=True)
+    book_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile=Profile(user=instance)
+        user_profile.save()
+post_save.connect(create_profile, sender=User)
