d7d13ec7b684360b358d72c64efe8c4ea9951f24
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-19 16:19:10 +0100

    serializer display wallet

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import Profile
-from fintech_app.models import Wallet
+from fintech_app.models import Wallet, WalletType
+from django.contrib.auth.models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Profile
-		fields = ('user', 'wallet', 'phone_no', 'profile_bio')
+    user = serializers.CharField(source='user.username')
+    wallet = serializers.SerializerMethodField()
+
+    def get_wallet(self, instance):
+        wallets = instance.wallet.all()
+        wallet_data = []
+        for wallet in wallets:
+            wallet_data.append({
+                'id': wallet.id,
+                'type': wallet.type,
+            })
+        return wallet_data
+
+    class Meta:
+        model = Profile
+        fields = ('user', 'wallet', 'phone_no', 'profile_bio')
 
 
 class AccountSerializer(serializers.ModelSerializer):
