f8d39fc93301b535aee3c5a82ed0ceb0bcf69fd9
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-28 12:31:15 +0100

    added pin-field to transc model

--- fintech_app/models.py
+++ fintech_app/models.py
@@ -37,6 +37,7 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255, default="")
     created_at = models.DateTimeField(auto_now_add=True)
+    pin = models.CharField(max_length=20, default="0000", editable=False)
 
     def __str__(self):
         return f'{self.wallet.user.username} to {self.receiver.user.username}'
